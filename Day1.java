5f9d6089f0b7b5517e55ab66e4a4a250d89c271e
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-02 21:21:34 +0800

    Day1.java

---
+++ Day1.java
@@ -0,0 +1,5 @@
+public class Day1 {//Deklarasi class yg namanya Day1
+    public static void main(String[] args) {//Method utama, titik awal program dijalankan
+        System.out.println("Latihan Day 1");//Mencetak teks ke layar
+    }//Penutup Method main
+}//penutup class day1
