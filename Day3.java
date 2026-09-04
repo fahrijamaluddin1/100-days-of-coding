83321fc735cd1bf771a2df13a0ad433a17194d80
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-04 11:24:27 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,23 @@
+public class Day3 { //Deklarasi class yg namanya Day1
+    public static void main(String[] args) {//Method utama, titik awal program dijalankan
+        // \n - baris baru
+        System.out.println("Baris pertama\nBaris kedua");
+
+        // \t - tab
+        System.out.println("Nama:\tBudi Santoso");
+        System.out.println("Umur:\t20 tahun");
+
+        // \" - tanda kutip ganda dalam string
+        System.out.println("Guru berkata, \"Kerjakan tugas dengan teliti!\"");
+
+        // \' - tanda kutip tunggal dalam string
+        System.out.println('a'); // langsung pakai char, hasil: 'a'
+        System.out.println("Contoh karakter: \'A\'");
+
+        // \\ - menampilkan backslash
+        System.out.println("Lokasi folder: D:\\Tugas\\Java\\Latihan");
+
+        // \r - carriage return (kursor kembali ke awal baris)
+        System.out.println("Halo Dunia\rSelamat");
+    }//penutup method main
+} //penutup class day1
