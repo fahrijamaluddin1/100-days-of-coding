520fc1937e5deacfb68d4e510f86ae40641ffbbc
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-05 14:34:44 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,15 @@
+public class Day4 {//Deklarasi class yg namanya Day4
+
+    public static void main(String[] args) {//method utama, titk awal program
+        byte umur = 19;//tipe data byte(angka kecil, -128/128)
+        short beratbadan = 168;//tipe data short (angka menengah, -32.768-32.767)
+        int tinggibadan = 68;//tipe data int (angka bulat besar =2700000)
+        float ukuransepatu = 40;//tipe data float ( angka desimal)
+
+        System.out.println("masukkan umur :"+ umur);//cetak teks+ nilai  umur
+          System.out.println("masukkan berat badan :"+ beratbadan);//cetak teks+nilai BB
+        System.out.println("masukkan tinngi badan :"+ tinggibadan);//cetak teks+nilai TB
+        System.out.println("ukuran sepatu? :"+ ukuransepatu);//cetak teks+ nilai UKS
+
+    }//penutup method main
+}//pentup class day4.
