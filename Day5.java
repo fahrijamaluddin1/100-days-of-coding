92760144f69995dccfff552d038484d36758e829
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-06 19:16:52 +0800

    Day5.java

--- Day5.java
+++ Day5.java
@@ -1,7 +1,7 @@
 public class Day5 {//Deklarasi class yg namanya Day5
     public static void main(String[] args) {//Method main, ini titik awal program dijalankan
-        Double IPK = 3.135;//Membuat variabel bernama IPK, tipe datanya Double dan isi nilai 3.135
-        float Lamawaktumakan = 10;//Membuat variabel bernama Lamawaktumakan, tipe datanya float, diisi nilai 10.
+        Double IPK = 3.135d;//Membuat variabel bernama IPK, tipe datanya Double dan isi nilai 3.135
+        float Lamawaktumakan = 10f;//Membuat variabel bernama Lamawaktumakan, tipe datanya float, diisi nilai 10.
         System.out.printf("Masukkan IPK :%.2f%n", IPK);//mencetak ke layar "masukkan IPK" digabung nilai IPK dengan 2 angka di belakang koma
         System.out.printf("Lama waktu makan :%.2fMenit%n", Lamawaktumakan);//mencetak teks, digabung nilai Lamawaktumakan dengan 2 angka di belakang koma, digabung teks "Menit"
     }//penutup method main
