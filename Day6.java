820375e18d7ec5d84e8799bacc136d0e53ed4204
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-07 19:02:37 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,9 @@
+public class Day6 {//Deklarasiclass yg namanya Day6
+    public static void main(String[] args) {//method main, ini titik awal program dijalankan
+        char nilai = 'A';//char 1 karakter
+        Boolean lulus = true;//boolean berfungsi untuk menambahkan "true/false"
+
+        System.out.println("masukkan nilai:"+ nilai);//mencetak ke layar "masukkan nilai"
+        System.out.println("apakah lulus? :"+lulus);//mencetak kelayar "apakah lulus?"
+    }//penutup method main
+}//penutup class day 6
