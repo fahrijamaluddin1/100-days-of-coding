c23cdf93d0b73a7cfc507d7ae83590c15cd821e2
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-08 14:50:21 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,7 @@
+public class Day7 {
+public static void main(String[] args) {
+    String a = "Assalamualaikum  warahmatullahi wabarakatuh, nama saya Fahri jamaluddin";//String,  berfungsi untuk menampung data yg bertipe teks. 
+
+    System.out.println(a);
+}
+}
