50b8df4bd43b60b0a23720a75c1b7f5d631aca27
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-09 12:56:49 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,9 @@
+public class Day8 {
+    public static void main(String[] args) {
+        double nilaiMahasiswa ;//mengubah/update variabel nilai mahasiswa
+        nilaiMahasiswa =  70.5d;
+        System.out.println("nilai sebelum diubah:"+ nilaiMahasiswa);
+        nilaiMahasiswa = 80.00d;
+        System.out.println("nilai stelah diubah: " +nilaiMahasiswa); 
+    }
+}
