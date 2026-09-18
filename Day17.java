fb4f738c92fee96443f0f7c326a6e1c34042c1c9
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-18 18:45:36 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,21 @@
+public class Day17 {
+    public static void main(String[] args) {
+        int angka = 10;
+        System.out.println("Nilai awal: " + angka);
+
+        angka += 5; 
+        System.out.println("Setelah += 5: " + angka);
+
+        angka -= 3; 
+        System.out.println("Setelah -= 3: " + angka);
+
+        angka *= 2; 
+        System.out.println("Setelah *= 2: " + angka);
+
+        angka /= 4; 
+        System.out.println("Setelah /= 4: " + angka);
+
+        angka %= 4; 
+        System.out.println("Setelah %= 4: " + angka);
+    }
+}
