3f24bc609a55b78d8e2ac5e32ca9e24fd7d4efe4
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-19 21:38:01 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,16 @@
+public class Day18 {
+    public static void main(String[] args) {
+
+        byte nilaiByte = 10;
+        int nilaiInt = nilaiByte;     
+        long nilaiLong = nilaiInt;     
+        float nilaiFloat = nilaiLong;   
+        double nilaiDouble = nilaiFloat; 
+
+        System.out.println("byte   : " + nilaiByte);
+        System.out.println("int    : " + nilaiInt);
+        System.out.println("long   : " + nilaiLong);
+        System.out.println("float  : " + nilaiFloat);
+        System.out.println("double : " + nilaiDouble);
+    }
+}
