c12ae4317e36cd8792ee9b4bd6d53e246a373fc6
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-20 21:48:10 +0800

    Day19

---
+++ Dst19.java
@@ -0,0 +1,21 @@
+public class Day19 {
+    public static void main(String[] args) {
+    
+
+        double nilaiDouble = 99.99;
+        int nilaiInt = (int) nilaiDouble; 
+        System.out.println("double ke int: " + nilaiInt); 
+
+        long nilaiLong = 123456789L;
+        int nilaiIntDariLong = (int) nilaiLong; 
+        System.out.println("long ke int: " + nilaiIntDariLong);
+
+        int nilaiBesar = 300;
+        byte nilaiByte = (byte) nilaiBesar; 
+        System.out.println("int ke byte: " + nilaiByte); 
+
+        int kodeAngka = 65;
+        char karakter = (char) kodeAngka; 
+        System.out.println("int ke char: " + karakter); 
+    }
+                            }
