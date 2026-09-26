f7245d9d8fb34dc9f6f808272b016b6603e808a7
fahrijamaluddin1 <fahrijamaluddin185@gmail.com>
2026-09-26 19:23:55 +0800

    Day25.java

--- Day25.java
+++ Day25.java
@@ -3,10 +3,21 @@ import java.util.Scanner;
 public class Day25 {
     public static void main(String[] args) {
         Scanner a = new Scanner(System.in);
-        System.out.print("Masukkan jari-jari : ");
-        double jari = a.nextDouble();
 
-        double hasil = (Math.PI * jari* jari);
-System.out.println("luas lingkaran : " +hasil);
+        System.out.print("Masukkan jari-jari : ");
+        String input = a.nextLine();
+
+        int jariBulat = Integer.parseInt(input);
+        double jari = (double) jariBulat;
+
+        final double PI = 3.14;
+
+        double hasil = PI * jari * jari;
+
+        hasil = hasil - (hasil * 2);
+
+        String output = String.valueOf(hasil);
+
+        System.out.printf("Luas lingkaran : %s%n", output);
     }
 }
